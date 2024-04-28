74c25fa2a74b9e69444c717cdd3c8d3a9e4b88ac
Md Main Uddin <2021md.main@gmail.com>
2024-04-28 09:50:46 +0600

    mobile nav bar fixes

--- src/components/Dashboard/NavBar/NavBar.tsx
+++ src/components/Dashboard/NavBar/NavBar.tsx
@@ -1,13 +1,15 @@
 'use client';
 import Styles from './NavBar.module.css';
-import { Button, ConfigProvider, Switch, message } from 'antd';
+import { Button, ConfigProvider, Drawer, Switch, message } from 'antd';
 import { getFromLocalStorage, setToLocalStorage } from '@/utils/local-storage';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { addBasicData } from '@/redux/slice/basicSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { usePathname, useRouter } from 'next/navigation';
 import { getAuthInfo } from '@/utils/jwt';
+import { CloseCircleOutlined, MenuOutlined } from '@ant-design/icons';
+import type { DrawerProps, RadioChangeEvent } from 'antd';
 
 const NavBar = () => {
 	const { basicData } = useAppSelector((state) => state.basicSlice);
@@ -17,10 +19,20 @@ const NavBar = () => {
 	const getSetLanguage = getFromLocalStorage('lang');
 	const router = useRouter();
 	const pathname = usePathname();
+	const [open, setOpen] = useState(false);
 	const pathArr = pathname.split('/');
 	const currentPage = pathArr[pathArr.length - 1];
 	const showLoginBtn =
 		pathname == '/login' || pathname == '/register' ? false : true;
+
+	const showDrawer = () => {
+		setOpen(true);
+	};
+
+	const onClose = () => {
+		setOpen(false);
+	};
+
 	useEffect(() => {
 		if (!getSetLanguage) {
 			setToLocalStorage('lang', 'বাং');
@@ -48,77 +60,101 @@ const NavBar = () => {
 	return (
 		<div className={Styles.container}>
 			{userInfo ? (
-				<div className={Styles.nav_container}>
-					<Link
-						href={'/dashboard'}
-						className={`${Styles.nav_item} ${currentPage === 'dashboard' && Styles.selected_item}`}
-					>
-						{getLang === 'বাং' ? 'ড্যাশবোর্ড' : 'Dashboard'}
-					</Link>
-					<Link
-						href={'/dashboard/orders'}
-						className={`${Styles.nav_item} ${currentPage === 'orders' && Styles.selected_item}`}
-					>
-						{getLang === 'বাং' ? 'অর্ডার' : 'Orders'}
-					</Link>
-					<Link
-						href={'/dashboard/history'}
-						className={`${Styles.nav_item} ${currentPage === 'history' && Styles.selected_item}`}
-					>
-						{getLang === 'বাং' ? 'হিস্টোরি' : 'History'}
-					</Link>
-					<Link
-						href={'/dashboard/transaction'}
-						className={`${Styles.nav_item} ${currentPage === 'transaction' && Styles.selected_item}`}
-					>
-						{getLang === 'বাং' ? 'লেনদেন' : 'Transaction'}
-					</Link>
-					<div className={Styles.login_container}>
-						<ConfigProvider
-							theme={{
-								token: {
-									colorPrimary: '#387849',
-									colorText: '#ffffff',
-									colorTextQuaternary: '#b5ccbf',
-								},
-								components: {
-									Switch: {},
-								},
-							}}
+				<>
+					<div className={Styles.nav_container}>
+						<Link
+							href={'/dashboard'}
+							className={`${Styles.nav_item} ${currentPage === 'dashboard' && Styles.selected_item}`}
 						>
-							<Switch
-								checkedChildren={
-									<div style={{ color: 'white', fontWeight: 'bold' }}>বাং</div>
-								}
-								unCheckedChildren={
-									<div style={{ fontWeight: 'bold' }}>ENG</div>
-								}
-								value={basicData.lang == 'eng' ? false : true}
-								onChange={(value) => handleChangeLanguage(value)}
-							/>
-						</ConfigProvider>
-						{userInfo ? (
+							{getLang === 'বাং' ? 'ড্যাশবোর্ড' : 'Dashboard'}
+						</Link>
+						<Link
+							href={'/dashboard/orders'}
+							className={`${Styles.nav_item} ${currentPage === 'orders' && Styles.selected_item}`}
+						>
+							{getLang === 'বাং' ? 'অর্ডার' : 'Orders'}
+						</Link>
+						<Link
+							href={'/dashboard/history'}
+							className={`${Styles.nav_item} ${currentPage === 'history' && Styles.selected_item}`}
+						>
+							{getLang === 'বাং' ? 'হিস্টোরি' : 'History'}
+						</Link>
+						<Link
+							href={'/dashboard/transaction'}
+							className={`${Styles.nav_item} ${currentPage === 'transaction' && Styles.selected_item}`}
+						>
+							{getLang === 'বাং' ? 'লেনদেন' : 'Transaction'}
+						</Link>
+						<div className={Styles.login_container}>
+							<ConfigProvider
+								theme={{
+									token: {
+										colorPrimary: '#387849',
+										colorText: '#ffffff',
+										colorTextQuaternary: '#b5ccbf',
+									},
+									components: {
+										Switch: {},
+									},
+								}}
+							>
+								<Switch
+									checkedChildren={
+										<div style={{ color: 'white', fontWeight: 'bold' }}>
+											বাং
+										</div>
+									}
+									unCheckedChildren={
+										<div style={{ fontWeight: 'bold' }}>ENG</div>
+									}
+									value={basicData.lang == 'eng' ? false : true}
+									onChange={(value) => handleChangeLanguage(value)}
+								/>
+							</ConfigProvider>
 							<p
 								onClick={handleLogout}
 								className={Styles.nav_item}
 								style={{ cursor: 'pointer' }}
 							>
-								Logout
+								{getLang === 'বাং' ? 'লগআউট' : 'Logout'}
 							</p>
-						) : (
-							<>
-								{showLoginBtn && (
-									<Button
-										onClick={() => router.push('/login')}
-										className={Styles.login_button}
-									>
-										Login
-									</Button>
-								)}
-							</>
-						)}
+						</div>
 					</div>
-				</div>
+					<div className={Styles.mobile_nav_container}>
+						<MenuOutlined onClick={() => setOpen(true)} />
+						<ConfigProvider
+							theme={{
+								token: {
+									colorBgMask: '#b5ccbf3f',
+								},
+							}}
+						>
+							<Drawer
+								style={{ backgroundColor: '#e7e3ce' }}
+								title={<div></div>}
+								placement={'left'}
+								width={250}
+								closeIcon={
+									<CloseCircleOutlined
+										style={{
+											position: 'absolute',
+											right: '15px',
+											top: '7px',
+											fontSize: '21px',
+										}}
+									/>
+								}
+								onClose={onClose}
+								open={open}
+							>
+								<p>Some contents...</p>
+								<p>Some contents...</p>
+								<p>Some contents...</p>
+							</Drawer>
+						</ConfigProvider>
+					</div>
+				</>
 			) : (
 				<div className={Styles.login_container}>
 					<ConfigProvider
@@ -142,13 +178,17 @@ const NavBar = () => {
 							onChange={(value) => handleChangeLanguage(value)}
 						/>
 					</ConfigProvider>
-					<Link
-						href={'/login'}
-						className={`${Styles.nav_item}`}
-						style={{ cursor: 'pointer' }}
-					>
-						{getLang === 'বাং' ? 'লগইন' : 'Login'}
-					</Link>
+					{showLoginBtn ? (
+						<Link
+							href={'/login'}
+							className={`${Styles.nav_item}`}
+							style={{ cursor: 'pointer' }}
+						>
+							{getLang === 'বাং' ? 'লগইন' : 'Login'}
+						</Link>
+					) : (
+						<></>
+					)}
 				</div>
 			)}
 		</div>
