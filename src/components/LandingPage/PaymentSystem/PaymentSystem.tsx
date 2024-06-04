740a04ba72a34e10868eddc5c33f005ae8d4ec2d
Md Main Uddin <2021md.main@gmail.com>
2024-06-04 09:51:26 +0600

    fixes

--- src/components/LandingPage/PaymentSystem/PaymentSystem.tsx
+++ src/components/LandingPage/PaymentSystem/PaymentSystem.tsx
@@ -1,49 +1,77 @@
+'use client';
 import { ArrowRightOutlined, CheckCircleOutlined } from '@ant-design/icons';
 import Styles from './style.module.css';
 import Image from 'next/image';
 import payment_img from '@/assets/payment.png';
+import { useAppSelector } from '@/redux/hooks';
 
 const PaymentSystem = () => {
+	const { basicData } = useAppSelector((state) => state.basicSlice);
+	const getLang = basicData.lang;
 	return (
 		<div className={Styles.container}>
-			<h1 className={Styles.header}>How To Pay us</h1>
-			<h2 style={{ textAlign: 'center' }}>We follow prepaid payment system</h2>
+			<h1 className={Styles.header}>
+				{getLang == 'বাং' ? 'কিভাবে বিল পে করবেন ?' : 'How To Pay us'}
+			</h1>
+			<h2 style={{ textAlign: 'center' }}>
+				{getLang == 'বাং'
+					? 'আমরা প্রিপেইড পেমেন্ট সিস্টেম অনুসরণ করি। আপনি আমাদের পেমেন্ট করার বিভিন্ন অপশন পাবেন।'
+					: 'We follow prepaid payment system. You will get multiple options to pay us.'}
+			</h2>
 			<div className={Styles.delivery_terms_container}>
 				<div className={Styles.img_container}>
 					<Image src={payment_img} alt="delivery_img" layout="responsive" />
 				</div>
 				<div>
 					<div className={Styles.condition_title}>
-						There are multiple ways to make your payment{' '}
+						{getLang == 'বাং'
+							? 'আপনার পেমেন্ট করার জন্য এখানে রয়েছে একাধিক উপায়'
+							: 'Here are multiple ways to make your payment'}{' '}
 						<ArrowRightOutlined
 							style={{ fontSize: '17px', color: 'var(--primary-color)' }}
 						/>
 					</div>
 					<div className={Styles.condition_container}>
-						<p>
+						<div className={Styles.options}>
 							<CheckCircleOutlined
 								style={{ fontSize: '17px', color: 'var(--primary-color)' }}
 							/>{' '}
-							Via Mobile Banking
-						</p>
-						<p>
+							<p>
+								{getLang == 'বাং'
+									? 'ডেলিভারি কর্মীর মাধ্যমে'
+									: 'Via delivery men'}
+							</p>
+						</div>
+						<div className={Styles.options}>
 							<CheckCircleOutlined
 								style={{ fontSize: '17px', color: 'var(--primary-color)' }}
 							/>{' '}
-							Direct to the admin
-						</p>
-						<p>
+							<p>
+								{getLang == 'বাং'
+									? 'প্রশাসকের কাছে সরাসরি'
+									: 'Direct to the admin'}
+							</p>
+						</div>
+						<div className={Styles.options}>
 							<CheckCircleOutlined
 								style={{ fontSize: '17px', color: 'var(--primary-color)' }}
 							/>{' '}
-							Via supplier
-						</p>
-						<p>
+							<p>
+								{getLang == 'বাং'
+									? 'মোবাইল ব্যাংকিংয়ের মাধ্যমে'
+									: 'Via Mobile Banking'}
+							</p>
+						</div>
+						<div className={Styles.options}>
 							<CheckCircleOutlined
 								style={{ fontSize: '17px', color: 'var(--primary-color)' }}
 							/>{' '}
-							Via bank transfer
-						</p>
+							<p>
+								{getLang == 'বাং'
+									? 'ব্যাংক ট্রান্সফারের মাধ্যমে'
+									: 'Via bank transfer'}
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
